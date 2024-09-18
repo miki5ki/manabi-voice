4753a471124663eb2e82aa3583dec0bc97de2816
miki5ki <53338596+miki5ki@users.noreply.github.com>
2024-09-18 11:09:49 +0900

    ✨ categoriesのルーティングを追加

---
+++ src/app/categories/[id]/edit/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>カテゴリの編集ページです</div>;
+};
+
+export default page;
---
+++ src/app/categories/[id]/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>カテゴリーの詳細ページです</div>;
+};
+
+export default page;
