5401c6abf01b6c2cd7db3e3021e5a3fae36e4699
miki5ki <53338596+miki5ki@users.noreply.github.com>
2024-09-18 11:10:08 +0900

    ✨ channelsのルーティングを追加

---
+++ src/app/channels/[id]/edit/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>チャンネルの編集ページです</div>;
+};
+
+export default page;
---
+++ src/app/channels/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>チャンネルの一覧ページです</div>;
+};
+
+export default page;
