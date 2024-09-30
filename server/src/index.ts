4d6ad897cead22fd28a90dff51e3325bd7e76208
miki5ki <53338596+miki5ki@users.noreply.github.com>
2024-09-30 20:19:32 +0900

    ✨ prismaの導入

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,4 +4,8 @@ import express from "express";
 const app: Express = express();
 const PORT = 8080;
 
+// app.get("/channels", (req: Request, res: Response) => {
+//   return res.send("channelsの一覧のレスポンスです");
+// });
+
 app.listen(PORT, () => console.log("server is running🚀"));
