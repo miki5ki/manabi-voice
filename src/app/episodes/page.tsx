ab3466c3659f469e291feb66c07dfb0daea21d26
miki5ki <53338596+miki5ki@users.noreply.github.com>
2024-11-09 13:25:32 +0900

    ✨ episodeの一覧ページの追加

--- src/app/episodes/page.tsx
+++ src/app/episodes/page.tsx
@@ -1,5 +1,41 @@
-const EpisodesPage = () => {
-  return <div>エピソードの一覧ページです</div>;
+import { Episode } from "@prisma/client";
+import Link from "next/link";
+
+import { getEpisodes } from "@/features/episodes/actions";
+
+const EpisodesPage = async () => {
+  const episodes = await getEpisodes();
+
+  if (!episodes) {
+    <p>エピソードがありません。</p>;
+  }
+
+  return (
+    <>
+      <Link href="/episodes/create">新規作成</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>タイトル</th>
+            <th>内容</th>
+          </tr>
+        </thead>
+        <tbody>
+          {episodes.map((episode: Episode) => (
+            <>
+              <tr key={episode.id}>
+                <td>{episode.title}</td>
+                <td>{episode.content}</td>
+                <td>
+                  <Link href={`/episodes/${episode.id}/edit`}>編集</Link>
+                </td>
+              </tr>
+            </>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default EpisodesPage;
