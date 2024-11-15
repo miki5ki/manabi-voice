4394e554d06ed5c6e8936e0c0c683c574acb7566
miki5ki <53338596+miki5ki@users.noreply.github.com>
2024-11-15 10:19:23 +0900

    ✨ 新規作成画面にカテゴリの選択機能を追加

--- src/app/channels/create/page.tsx
+++ src/app/channels/create/page.tsx
@@ -1,11 +1,20 @@
+import { getCategories } from "@/features/categories/actions";
 import { createChannel } from "@/features/channels/actions";
 
-const ChannelCreatePage = () => {
+const ChannelCreatePage = async () => {
+  const categories = await getCategories();
   return (
     <>
       <form action={createChannel}>
         <input type="text" name="channelTitle" placeholder="チャンネルタイトル" required />
         <input type="text" name="channelDescription" placeholder="チャンネル詳細" />
+        <select name="categoryId">
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.title}
+            </option>
+          ))}
+        </select>
         <button>作成</button>
       </form>
     </>
