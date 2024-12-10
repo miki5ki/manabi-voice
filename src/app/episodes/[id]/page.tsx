19ce758acf1c4193fd0857a0c3b7198682279782
miki5ki <53338596+miki5ki@users.noreply.github.com>
2024-12-10 10:16:25 +0900

    ✨ コメントの新規登録を追加

--- src/app/episodes/[id]/page.tsx
+++ src/app/episodes/[id]/page.tsx
@@ -1,5 +1,26 @@
-const EpisodeShowPage = () => {
-  return <div>エピソードの詳細ページです</div>;
+import { CreateComment } from "@/features/comments/actions";
+
+type Props = {
+  params: {
+    id: string;
+  };
+  searchParams: object;
+};
+const EpisodeShowPage = (props: Props) => {
+  const { params } = props;
+  const { id } = params;
+
+  return (
+    <>
+      <div>エピソードの詳細ページです</div>
+      <form method="POST" action={CreateComment}>
+        <input hidden value="8167baf4-b514-4f8e-9a43-d81bf9a8a305" name="userId" />
+        <input hidden value={id} name="episodeId" />
+        <input type="input" name="description"></input>
+        <button>投稿</button>
+      </form>
+    </>
+  );
 };
 
 export default EpisodeShowPage;
