126a6564a822827afeb356a3e87d09ce43a3bdb3
miki5ki <53338596+miki5ki@users.noreply.github.com>
2025-02-07 11:13:11 +0900

    ♻️ httpErrorのclassを追加

--- src/lib/prismaErrorHandler.ts
+++ src/lib/prismaErrorHandler.ts
@@ -1,28 +1,31 @@
 import { Prisma } from "@prisma/client";
 
+import { HttpError } from "./http-error";
+
 export const prismaErrorHandler = (e: unknown) => {
   if (e instanceof Prisma.PrismaClientKnownRequestError) {
     if (e.code === "P2002") {
-      return new Response("このデータはすでに存在しています", { status: 400 });
+      throw new HttpError(409, `エラーコード：${e.code} 「${e.meta?.target}」はすでにデータベースに登録されています`);
     }
     console.error("PrismaClientKnownRequestError:", e.code, e.message);
-    return new Response(`${e.code}のエラーが発生しました。${e.message}`, { status: 400 });
+    throw new HttpError(400, `エラーコード：${e.code} このデータはデータベースの制約条件に該当しています`);
   }
 
   if (e instanceof Prisma.PrismaClientValidationError) {
-    return new Response("リクエストの入力値が正しくありません", { status: 400 });
+    console.error("PrismaClientValidationError", e.message);
+    throw new HttpError(400, "バリデーションに失敗しました");
   }
 
   if (e instanceof Prisma.PrismaClientInitializationError) {
-    console.error("データベースの接続に失敗しました:", e);
-    return new Response("現在サーバーが利用できません。後でもう一度試してください。", { status: 500 });
+    console.error("PrismaClientInitializationError", e);
+    throw new HttpError(500, "データベースの接続に失敗しました。しばらく経ってから再度お試しください");
   }
 
   if (e instanceof Prisma.PrismaClientUnknownRequestError) {
-    console.error("Prismaの未知のエラーが発生しました:", e);
-    return new Response("予期しないエラーが発生しました。", { status: 500 });
+    console.error("PrismaClientUnknownRequestError", e);
+    throw new HttpError(500, "予期せぬエラーが発生しました");
   }
 
-  console.error("Database Error:", e);
-  return new Response("サーバーエラーが発生しました。", { status: 500 });
+  console.error("Database Error:", e instanceof Error ? e.stack : e);
+  throw new HttpError(500, "予期せぬエラーが発生しました");
 };
