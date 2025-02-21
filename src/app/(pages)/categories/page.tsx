f204e9b9ee76e2b090a2fc8205b48980ceebc626
miki5ki <53338596+miki5ki@users.noreply.github.com>
2025-02-21 10:05:39 +0900

    💄 アクションボタン系のuiを追加

--- src/app/(pages)/categories/page.tsx
+++ src/app/(pages)/categories/page.tsx
@@ -1,5 +1,5 @@
-import { Grid2 } from "@mui/material";
-import Link from "next/link";
+import { Search } from "@mui/icons-material";
+import { Button, Grid2, InputAdornment, TextField } from "@mui/material";
 
 import { getCategories } from "@/features/categories/actions";
 
@@ -10,8 +10,30 @@ const CategoriesPage = async () => {
 
   return (
     <>
-      <Link href={"/categories/create"}>新規作成</Link>
-      <Grid2 container spacing={2}>
+      <Grid2 container spacing={1} m={3}>
+        <TextField
+          variant="standard"
+          color="secondary"
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+        <Button variant="outlined" color="secondary">
+          検索
+        </Button>
+
+        <Button variant="contained" color="primary" href={"/categories/create"}>
+          新規作成
+        </Button>
+      </Grid2>
+
+      <Grid2 container spacing={2} m={3}>
         {categories.map((category) => (
           <CategoryCard {...category} key={category.id} />
         ))}
