d2eb8aebe98cd05d8f75b0d590a8a251f3f596fd
miki5ki <53338596+miki5ki@users.noreply.github.com>
2025-03-27 10:36:51 +0900

    💄 ヘッダーのデザインを更新

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,59 +1,40 @@
 import { Claims } from "@auth0/nextjs-auth0";
-import {
-  AppBar,
-  Avatar,
-  Box,
-  Button,
-  Container,
-  IconButton,
-  Menu,
-  MenuItem,
-  Toolbar,
-  Tooltip,
-  Typography,
-} from "@mui/material";
+import { AppBar, Avatar, Box, Button, Container, Toolbar } from "@mui/material";
 import { notFound } from "next/navigation";
 
 import { getUser } from "@/features/users/actions";
+
+import { Settings } from "./Settings";
 
 export const Header = async (props: Claims) => {
   const { user } = props;
   const userProfile = await getUser(user.sub);
   if (!userProfile) notFound();
-
-  const pagesInfo = [
-    { id: 1, title: "カテゴリー", link: "/categories" },
-    { id: 2, title: "チャンネル", link: "/channels" },
-    { id: 3, title: "エピソード", link: "/episodes" },
-    { id: 4, title: "マイページ", link: `/users/${userProfile.id}` },
-    { id: 5, title: "ログアウト", link: "/api/auth/logout" },
+  const pages = [
+    { title: "カテゴリー", link: "/categories" },
+    { title: "チャンネル", link: "/channels" },
+    { title: "エピソード", link: "/episodes" },
   ];
 
   return (
-    <AppBar position="static" color="secondary">
-      <Container maxWidth="xl">
-        <Toolbar>
-          <Box>
-            {pagesInfo.map((pageInfo) => (
-              <Button key={pageInfo.id} href={pageInfo.link} color="inherit">
-                {pageInfo.title}
-              </Button>
-            ))}
+    <>
+      <AppBar position="static" color="secondary">
+        <Container maxWidth="xl">
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Toolbar>
+              <Avatar src="/logo.png" alt="ロゴ" sx={{ height: 52, mr: 3, width: 52 }} />
+              <Box>
+                {pages.map((page) => (
+                  <Button key={page.link} href={page.link} color="inherit">
+                    {page.title}
+                  </Button>
+                ))}
+              </Box>
+            </Toolbar>
+            <Settings userProfile={userProfile} />
           </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton>
-                <Avatar alt="Remy Sharp" src="https://picsum.photos/id/236/360/180?grayscale" />
-              </IconButton>
-            </Tooltip>
-            <Menu open={true}>
-              <MenuItem>
-                <Typography sx={{ textAlign: "center" }}>aaaaaa</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+        </Container>
+      </AppBar>
+    </>
   );
 };
