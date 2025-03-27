ba6779f850b0fddc30441b5cd5e80b1127862a19
miki5ki <53338596+miki5ki@users.noreply.github.com>
2025-03-27 10:55:52 +0900

    💄 ヘッダーのUIを実装

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,40 @@
 import { Claims } from "@auth0/nextjs-auth0";
-import Link from "next/link";
+import { AppBar, Avatar, Box, Button, Container, Toolbar } from "@mui/material";
 import { notFound } from "next/navigation";
 
 import { getUser } from "@/features/users/actions";
+
+import { Settings } from "./Settings";
 
 export const Header = async (props: Claims) => {
   const { user } = props;
   const userProfile = await getUser(user.sub);
   if (!userProfile) notFound();
+  const pages = [
+    { title: "カテゴリー", link: "/categories" },
+    { title: "チャンネル", link: "/channels" },
+    { title: "エピソード", link: "/episodes" },
+  ];
 
   return (
-    <div>
-      <Link href={`/users/${userProfile.id}`}>{user.name}</Link>
-      <span>これは仮のヘッダーです</span>
-      <a href="/api/auth/logout">Logout</a>
-    </div>
+    <>
+      <AppBar position="static" color="secondary">
+        <Container maxWidth="xl">
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Toolbar>
+              <Avatar src="/logo.png" alt="ロゴ" sx={{ height: 52, mr: 3, width: 52 }} />
+              <Box>
+                {pages.map((page) => (
+                  <Button key={page.link} href={page.link} color="inherit">
+                    {page.title}
+                  </Button>
+                ))}
+              </Box>
+            </Toolbar>
+            <Settings userProfile={userProfile} />
+          </Box>
+        </Container>
+      </AppBar>
+    </>
   );
 };
