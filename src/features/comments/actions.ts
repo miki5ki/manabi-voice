f451434dc3997fa9e848649d744fdad3527853b2
miki5ki <53338596+miki5ki@users.noreply.github.com>
2025-04-04 09:22:45 +0900

    ✨ getComments時にuserNameを一緒に取得するように変更

--- src/features/comments/actions.ts
+++ src/features/comments/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { z } from "zod";
 
 import { prisma } from "@/lib/prisma";
@@ -37,15 +38,18 @@ export async function CreateComment(formData: FormData) {
   revalidatePath(`/episodes${validComment.episodeId}`);
 }
 
-export async function getComments(id: string) {
+export async function getComments(episodeId: string) {
   try {
     const res = await prisma.comments.findMany({
+      include: {
+        user: {
+          select: { name: true },
+        },
+      },
       orderBy: {
         createdAt: "desc",
       },
-      where: {
-        episodeId: id,
-      },
+      where: { episodeId },
     });
     return res;
   } catch (e) {
