53a668253d78885826c344a5c2f4c40c3f3601f0
miki5ki <53338596+miki5ki@users.noreply.github.com>
2025-04-11 16:39:18 +0900

    💄 チャネルの詳細画面を刷新

--- src/app/(pages)/channels/[id]/page.tsx
+++ src/app/(pages)/channels/[id]/page.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Episode } from "@prisma/client";
+import { notFound } from "next/navigation";
 
-const ChannelShowPage = () => {
-  return <div>チャンネルの詳細ページです</div>;
+import { WideCard } from "@/app/components/WideCard";
+import { getChannel } from "@/features/channels/actions";
+import { getEpisodes } from "@/features/episodes/actions";
+
+type Props = {
+  params: {
+    id: string;
+  };
+  searchParams: object;
+};
+
+const ChannelShowPage = async (props: Props) => {
+  const { params } = props;
+  const { id } = params;
+  const channel = await getChannel(id);
+  const episodes = await getEpisodes();
+  if (!channel) notFound();
+
+  return (
+    <>
+      <Box>
+        <Typography variant="h4">{channel.title}</Typography>
+        <Typography variant="subtitle1">{channel.description}</Typography>
+      </Box>
+      <Box>
+        <Typography variant="h6">エピソード一覧</Typography>
+        <Stack spacing={5} mx={4}>
+          {episodes.map((episode: Episode) => (
+            <WideCard key={episode.id} {...episode} />
+          ))}
+        </Stack>
+      </Box>
+    </>
+  );
 };
 
 export default ChannelShowPage;
