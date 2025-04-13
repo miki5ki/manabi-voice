62278ca1e7c99b12118cc598a9f862b598f6f36c
miki5ki <53338596+miki5ki@users.noreply.github.com>
2025-04-13 15:55:06 +0900

    🐛 middlewareでnodeを動かすためにライブラリを更新及びコード書き換え

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,16 +1,21 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
-import { SessionData } from "@auth0/nextjs-auth0/types";
+import { NextResponse } from "next/server";
 
 import { upsertUser } from "@/features/users/actions";
 
 export const auth0 = new Auth0Client({
-  sessionStore: {
-    async delete() {},
-    async get() {
-      return null;
-    },
-    async set(id, sessionData: SessionData) {
-      await upsertUser(sessionData);
-    },
+  async onCallback(error, context, session) {
+    // redirect the user to a custom error page
+    if (error || !session?.user) {
+      return NextResponse.redirect(
+        new URL(`/error?error=${error?.message || "unknown error"}`, process.env.APP_BASE_URL),
+      );
+    }
+
+    // 他のカスタマイズログインロジック
+    await upsertUser(session);
+
+    // complete the redirect to the provided returnTo URL
+    return NextResponse.redirect(new URL(context.returnTo || "/", process.env.APP_BASE_URL));
   },
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -16,4 +16,5 @@ export const config = {
      */
     "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
+  runtime: "nodejs",
 };
