280784f2b8c747c4db2f253032b6fc701d0eb2cd
miki5ki <53338596+miki5ki@users.noreply.github.com>
2025-04-18 10:00:43 +0900

    💄 categoryの詳細ページを刷新

--- src/app/(pages)/categories/[id]/page.tsx
+++ src/app/(pages)/categories/[id]/page.tsx
@@ -1,6 +1,8 @@
-import { Grid2 } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
+import { notFound } from "next/navigation";
 
 import { ViewCard } from "@/app/components/ViewCard";
+import { getCategory } from "@/features/categories/actions";
 import { getChannelsByCategory, getEpisodesByCategory } from "@/features/categories/relations/actions";
 
 type Props = {
@@ -13,25 +15,31 @@ type Props = {
 const CategoryShowPage = async (props: Props) => {
   const { params } = props;
   const { id } = params;
-
+  const category = await getCategory(id);
+  if (!category) notFound();
   const channels = await getChannelsByCategory(id);
   const episodes = await getEpisodesByCategory(id);
   return (
     <>
-      <div>
-        <Grid2 container spacing={2} m={3}>
-          {channels.map((channel) => (
-            <ViewCard {...channel} viewType="channels" key={channel.id} />
-          ))}
-        </Grid2>
-      </div>
-      <div>
-        <Grid2 container spacing={2} m={3}>
-          {episodes.map((episode) => (
-            <ViewCard {...episode} viewType="episodes" key={episode.id} />
-          ))}
-        </Grid2>
-      </div>
+      <Typography variant="h4" m={3}>
+        {category.title}
+      </Typography>
+      <Typography m={3} variant="h6">
+        チャンネル
+      </Typography>
+      <Grid2 container spacing={2} m={3}>
+        {channels.map((channel) => (
+          <ViewCard {...channel} viewType="channels" key={channel.id} />
+        ))}
+      </Grid2>
+      <Typography m={3} variant="h6">
+        エピソード
+      </Typography>
+      <Grid2 container spacing={2} m={3}>
+        {episodes.map((episode) => (
+          <ViewCard {...episode} viewType="episodes" key={episode.id} />
+        ))}
+      </Grid2>
     </>
   );
 };
