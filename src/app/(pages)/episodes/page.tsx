79bba03fff23e3a9a87e0f3f8f63c9be819ed6a0
miki5ki <53338596+miki5ki@users.noreply.github.com>
2025-04-30 06:56:36 +0900

    ✨ 一覧ページに権限による新規作成のだし分けを追加

--- src/app/(pages)/episodes/page.tsx
+++ src/app/(pages)/episodes/page.tsx
@@ -5,7 +5,9 @@ import { notFound } from "next/navigation";
 
 import { ViewAction } from "@/app/components/ViewAction";
 import { WideCard } from "@/app/components/WideCard";
+import { getValidSession } from "@/features/auth/actions";
 import { getEpisodes } from "@/features/episodes/actions";
+import { hasPermission } from "@/lib/permission";
 
 const linkStyle = {
   textDecoration: "none",
@@ -14,12 +16,14 @@ const linkStyle = {
 const EpisodesPage = async ({ searchParams }: { searchParams: { keyWord?: string } }) => {
   const keyWord = searchParams.keyWord ?? "";
   const episodes = await getEpisodes({ keyWord: keyWord });
+  const session = await getValidSession();
+  const creatable = hasPermission(session.user.appUserRole, "episode:create");
 
   if (!episodes) notFound();
 
   return (
     <>
-      <ViewAction viewType="episodes" />
+      <ViewAction viewType="episodes" creatable={creatable} />
       <Stack spacing={5} mx={3}>
         {episodes.map((episode: Episode) => (
           <Link key={episode.id} href={`/episodes/${episode.id}`} passHref style={linkStyle}>
